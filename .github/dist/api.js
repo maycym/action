4f4c8d0ada4429d6da276ea08bcefb8b15e58d69
maycym <120078909+maycym@users.noreply.github.com>
2022-12-08 16:20:49 +0800

    Delete .github/dist directory

--- .github/dist/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});
-
-var _chunkGQNGCSXUjs = require('./chunk-GQNGCSXU.js');
-require('./chunk-OKMUBGTU.js');
-
-
-exports.ZjuHealthReporter = _chunkGQNGCSXUjs.ZjuHealthReporter;
-//# sourceMappingURL=api.js.map
